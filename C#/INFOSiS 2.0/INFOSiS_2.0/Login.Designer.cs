d66a26c51b1ce3fb515534fc4ee4d247acd6fb2c
alulab14 <alulab14@inf.pucp.edu.pe>
2019-06-19 21:28:22 -0500

    eddie ctmre no te escaparas de la justicia

--- C#/INFOSiS 2.0/INFOSiS_2.0/Login.Designer.cs
+++ C#/INFOSiS 2.0/INFOSiS_2.0/Login.Designer.cs
@@ -41,21 +41,24 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.Font = new System.Drawing.Font("Gill Sans MT", 40.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(12, 58);
+            this.label1.Location = new System.Drawing.Point(9, 47);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(408, 107);
+            this.label1.Size = new System.Drawing.Size(306, 87);
             this.label1.TabIndex = 0;
             this.label1.Text = "INFOSiS";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // btnLogIn
             // 
             this.btnLogIn.BackColor = System.Drawing.Color.MidnightBlue;
             this.btnLogIn.Font = new System.Drawing.Font("Gill Sans MT", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLogIn.ForeColor = System.Drawing.Color.White;
-            this.btnLogIn.Location = new System.Drawing.Point(68, 395);
+            this.btnLogIn.Location = new System.Drawing.Point(51, 321);
+            this.btnLogIn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLogIn.Name = "btnLogIn";
-            this.btnLogIn.Size = new System.Drawing.Size(290, 47);
+            this.btnLogIn.Size = new System.Drawing.Size(218, 38);
             this.btnLogIn.TabIndex = 1;
             this.btnLogIn.Text = "Acceder";
             this.btnLogIn.UseVisualStyleBackColor = false;
@@ -66,9 +69,10 @@
             this.lblUser.AutoSize = true;
             this.lblUser.Font = new System.Drawing.Font("Gill Sans MT", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblUser.ForeColor = System.Drawing.Color.White;
-            this.lblUser.Location = new System.Drawing.Point(62, 202);
+            this.lblUser.Location = new System.Drawing.Point(46, 164);
+            this.lblUser.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblUser.Name = "lblUser";
-            this.lblUser.Size = new System.Drawing.Size(91, 34);
+            this.lblUser.Size = new System.Drawing.Size(72, 27);
             this.lblUser.TabIndex = 2;
             this.lblUser.Text = "Usuario";
             // 
@@ -77,9 +81,10 @@
             this.lblPassword.AutoSize = true;
             this.lblPassword.Font = new System.Drawing.Font("Gill Sans MT", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblPassword.ForeColor = System.Drawing.Color.White;
-            this.lblPassword.Location = new System.Drawing.Point(62, 293);
+            this.lblPassword.Location = new System.Drawing.Point(46, 238);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(128, 34);
+            this.lblPassword.Size = new System.Drawing.Size(101, 27);
             this.lblPassword.TabIndex = 3;
             this.lblPassword.Text = "Contraseña";
             // 
@@ -87,9 +92,10 @@
             // 
             this.txtUser.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.txtUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUser.Location = new System.Drawing.Point(68, 243);
+            this.txtUser.Location = new System.Drawing.Point(51, 197);
+            this.txtUser.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtUser.Name = "txtUser";
-            this.txtUser.Size = new System.Drawing.Size(290, 27);
+            this.txtUser.Size = new System.Drawing.Size(218, 23);
             this.txtUser.TabIndex = 4;
             this.txtUser.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Login_KeyDown);
             // 
@@ -97,19 +103,20 @@
             // 
             this.txtPassword.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPassword.Location = new System.Drawing.Point(68, 339);
+            this.txtPassword.Location = new System.Drawing.Point(51, 275);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(290, 27);
+            this.txtPassword.Size = new System.Drawing.Size(218, 23);
             this.txtPassword.TabIndex = 5;
             this.txtPassword.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Login_KeyDown);
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(81)))), ((int)(((byte)(165)))));
-            this.ClientSize = new System.Drawing.Size(432, 512);
+            this.ClientSize = new System.Drawing.Size(324, 416);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.txtUser);
             this.Controls.Add(this.lblPassword);
@@ -117,6 +124,7 @@
             this.Controls.Add(this.btnLogIn);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Login";
             this.Text = "Bienvenido(a) a INFOSiS";
             this.Load += new System.EventHandler(this.Login_Load);
